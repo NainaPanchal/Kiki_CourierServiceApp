82d3fe2e580e55147305010a26f24210b1a39018
Naina <Nayanbalapanchal@yahoo.com>
2025-11-17 20:26:41 +0530

    --

--- Kiki_CourierServiceApp/Models/Package.cs
+++ Kiki_CourierServiceApp/Models/Package.cs
@@ -4,7 +4,9 @@ namespace Kiki_CourierServiceApp.Models
     {
         public string Id { get; set; } = string.Empty;
         public double Weight { get; set; }
+        //public string WeightUnit { get; set; } = string.Empty;
         public double Distance { get; set; }
+        //public string DistanceUnit { get; set; } = string.Empty;
         public string OfferCode { get; set; } = string.Empty;
         public double Discount { get; set; }
         public double TotalCost { get; set; }
